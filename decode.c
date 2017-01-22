3e7d9aa26a56f63c203acb0c0405403dbea1afc7
ddvv <529531385@qq.com>
2017-01-22 20:33:04 +0800

    lost the file last commit.

--- decode.c
+++ decode.c
@@ -9,16 +9,14 @@ int main(int argc, char *argv[])
 
     char *buffer = NULL;
     long file_length = 0;
+
     get_signal_data(sig_file, &buffer, &file_length);
-    long end = 0;
-    for(long i = 0; i < file_length; i += PACKET_SIZE)
-    {
-        end = PACKET_SIZE < (file_length - i) ? PACKET_SIZE : (file_length - i - 2);
-        memset(g_inter, 0, sizeof(g_inter));
-        mean(buffer, i, end);
-        find_inter(buffer, i, end);
-        work(buffer);
-    }
+    
+    memset(g_inter, 0, sizeof(g_inter));
+    mean(buffer, 0, file_length);
+    find_inter(buffer, 0, file_length);
+    work(buffer, NULL);
+
     printf("end.\n");
     //release the memory.
     release(buffer);
